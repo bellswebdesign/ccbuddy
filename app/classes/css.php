5aa2f5d73b4d19e71251f65c957bd749617f0503
bellswebdesign <alanbell2012@gmail.com>
2022-02-13 16:56:57 -0500

    Add files via upload

---
+++ app/classes/css.php
@@ -0,0 +1,4 @@
+<!-- Required CSS assets -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link href="assets/css/style.min.css" rel="stylesheet">
+<link href="assets/css/dashboard.css" rel=stylesheet">
