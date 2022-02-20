5540f366589b18d73bb6c7328aff87d5ac0b1d3a
bellswebdesign <alanbell2012@gmail.com>
2022-02-20 07:50:43 -0500

    Add files via upload

--- app/initialize.php
+++ app/initialize.php
@@ -4,9 +4,13 @@
 	ob_start();
 
 	// Classes
-    require_once('classes/class.Database.php');
+	require_once('classes/class.Ajax.php');
+	require_once('classes/class.Database.php');
 	require_once('classes/class.Harvest.php');
   require_once('classes/class.Harvest.Add.php');
+	require_once('classes/class.Strain.php');
+	require_once('classes/class.Strain.Add.php');
+	require_once('classes/class.Destem.php');
 
     // Global App Fucntions
     require_once('functions.php');
